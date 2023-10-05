87020ba33530d3c7fd389542cd27eb46ecf9e5fc
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2023-10-05 03:32:33 +0100

    added mongoDB cloud

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 
 // route modules
-const enroll = require("./routes/enroll")
-const verify = require("./routes/verify")
-
+const enroll = require("./routes/enroll");
+const verify = require("./routes/verify");
 
 // db connection
 mongoose
-   .connect("mongodb://localhost:27017/bioreg")
+   .connect(
+      "mongodb+srv://TTF:xG4zQ2GuePAVd5nU@cluster0.hbcbrlu.mongodb.net/?retryWrites=true&w=majority"
+   )
    .then(() => console.log("Databse connected successfully"))
    .catch((err) => console.log(err));
 
 // configs
 const app = express();
-app.use(express.json())
-app.use(cors({
-    "Access-Control-Allow-Origin": "http://localhost:3000"
-}))
+app.use(express.json());
+app.use(
+   cors({
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+   })
+);
 
 // ROUTES
-app.use('/enroll', enroll)
-app.use('/verify', verify)
+app.use("/enroll", enroll);
+app.use("/verify", verify);
 
 app.listen(process.env.PORT || 5000, () => {
    console.log("Listening to port 5000...");
